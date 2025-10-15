853dcb58d40262ad74b15dbd1e4cbec3c08f560d
Joel Kram <joelnskram@gmail.com>
2025-10-15 20:40:25 +0100

    remove turbopack as causing build error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //disable turbopack for build as causing error with vercell
+  turbo: false,
 };
 
 export default nextConfig;
