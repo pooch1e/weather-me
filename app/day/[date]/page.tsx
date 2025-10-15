56b1a60e726c117aed1036d3ba1aa3873a641455
Joel Kram <joelnskram@gmail.com>
2025-10-15 09:43:05 +0100

    add component for dayTemp + render via params

--- app/day/[date]/page.tsx
+++ app/day/[date]/page.tsx
@@ -1,6 +1,5 @@
 import { fetchWeatherData } from '@/app/lib/weather/WeatherService';
 import DailyWeatherCard from '@/app/ui/components/Weather/DailyWeather/DailyWeatherCard';
-import WeatherCard from '@/app/ui/components/Weather/WeatherCard';
 
 export default async function DayPage({
   params,
@@ -10,6 +9,7 @@ export default async function DayPage({
   const { date } = await params;
   try {
     const weatherData = await fetchWeatherData(51.5085, -0.1257); // get cached data
+
     // filter data for day
     const hourlyData = weatherData.hourly.filter((item) =>
       item.time.startsWith(date)
@@ -17,7 +17,8 @@ export default async function DayPage({
     //get temp for day
     const dailyData = weatherData.daily.find((item) => item.time === date);
 
+
     // return weather card with addition of day and temp
-    return <DailyWeatherCard hourlyData={hourlyData} dailyData={dailyData?.maxTemp} />;
+    return <DailyWeatherCard weatherCode={weatherData.current.weatherCode}hourlyData={hourlyData} dailyData={dailyData} />;
   } catch (err: unknown) {}
 }
