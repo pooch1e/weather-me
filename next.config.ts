3712a21f48a1fa850f0a4eeb1a04d6fb30dba622
Joel Kram <joelnskram@gmail.com>
2025-10-15 20:43:27 +0100

    remove lightning css dependency

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   //disable turbopack for build as causing error with vercell
   turbo: false,
+  experimental: {
+    optimizeCss: true,
+  },
 };
 
 export default nextConfig;
