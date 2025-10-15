95ac490133acd6bf2a7a987e506f4289639da535
Joel Kram <joelnskram@gmail.com>
2025-10-15 09:16:21 +0100

    add day page and filter for daily data

--- app/day/[date]/page.tsx
+++ app/day/[date]/page.tsx
@@ -1,15 +1,23 @@
-import { fetchWeatherData } from "@/app/lib/weather/WeatherService";
+import { fetchWeatherData } from '@/app/lib/weather/WeatherService';
+import DailyWeatherCard from '@/app/ui/components/Weather/DailyWeather/DailyWeatherCard';
+import WeatherCard from '@/app/ui/components/Weather/WeatherCard';
 
-export default async function DayPage(({ params }: { params: { date: string } }) {
-  const {date} = params;
+export default async function DayPage({
+  params,
+}: {
+  params: { date: string };
+}) {
+  const { date } = await params;
   try {
     const weatherData = await fetchWeatherData(51.5085, -0.1257); // get cached data
     // filter data for day
+    const hourlyData = weatherData.hourly.filter((item) =>
+      item.time.startsWith(date)
+    );
+    //get temp for day
+    const dailyData = weatherData.daily.find((item) => item.time === date);
+
     // return weather card with addition of day and temp
-    
-  } catch (err: unknown) {
-    
-  }
-
-
+    return <DailyWeatherCard hourlyData={hourlyData} dailyData={dailyData?.maxTemp} />;
+  } catch (err: unknown) {}
 }
---
+++ app/ui/components/Weather/DailyWeather/DailyWeatherCard.tsx
@@ -0,0 +1,13 @@
+'use client';
+interface DailyWeatherProps {
+  hourlyData: Array<string>;
+  dailyData: Array<number>;
+}
+export default function DailyWeatherCard({
+  hourlyData,
+  dailyData,
+}: DailyWeatherProps) {
+  return <div>
+    
+  </div>;
+}
