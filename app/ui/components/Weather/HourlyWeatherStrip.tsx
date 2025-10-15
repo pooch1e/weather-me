dd4547abe49dd7dcc636e56faa4ce6fad52cece2
Joel Kram <joelnskram@gmail.com>
2025-10-15 13:58:33 +0100

    add icons to hourly scroll component

--- app/ui/components/Weather/HourlyWeatherStrip.tsx
+++ app/ui/components/Weather/HourlyWeatherStrip.tsx
@@ -21,18 +21,48 @@ export default function HourlyWeatherStrip({ hourlyData }: HourlyWeather) {
   };
   return (
     <div className="bg-gray-900 border border-green-400 rounded-2xl p-6 shadow-lg">
-      <div className="overflow-x-auto">
+      <div className="overflow-x-auto no-scrollbar">
         <div className="flex space-x-6">
           {hourlyData.slice(0, 24).map((hour, index) => (
             <div
               key={index}
               className="flex flex-col items-center space-y-2 text-center min-w-16">
-              <span className="text-xs text-gray-300 tracking-wide">{formatTime(hour.time)}</span>
+              <span className="text-xs text-gray-300 tracking-wide">
+                {formatTime(hour.time)}
+              </span>
               <span className="text-sm font-light text-white">
                 {Math.round(hour.temperature)}°
               </span>
             </div>
           ))}
+        </div>
+        <div className="flex justify-between">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+            />
+          </svg>
         </div>
       </div>
     </div>
